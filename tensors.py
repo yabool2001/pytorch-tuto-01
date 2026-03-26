0f12d693e1e30eebfccba28f13d693f38ca6fe84
yabool2001 <mzemlo.pl@gmail.com>
2026-03-26 22:38:52 +0100

    aa

--- tensors.py
+++ tensors.py
@@ -1,3 +1,13 @@
 import torch
 
+import numpy as np
+from numpy.typing import NDArray
+
+barker13_filename = "barker13_samples_clipped.npy"
+n : NDArray [ np.complex128 ] = np.load ( barker13_filename ).astype ( np.complex128 , copy = False )
+
 x = torch.rand ( 2 , 2 , dtype = torch.float16 , requires_grad = True )
+y = torch.from_numpy ( n ).to ( torch.complex128 )
+
+print ( x )
+print ( y )
